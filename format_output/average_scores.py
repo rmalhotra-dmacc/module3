3aca4ece38db5f5055318d4eb5f9601096df1995
Rajiv Malhotra <malhotra.rajiv@principal.com>
2020-09-14 14:12:42 -0500

    PEP-8 Compliant

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -7,11 +7,13 @@ The program to  read in a person's names, last and first, their age and three sc
 Take the three scores and find the average, storing into a variable
 """""
 
+
 def average():
     score1 = input("Enter your first score:")
     score2 = input("Enter your second score:")
     score3 = input("Enter your third score:")
-    return (int(score1) + int(score2) + int(score3))/3
+    return (int(score1) + int(score2) + int(score3)) / 3
+
 
 if __name__ == '__main__':
     lastName = input("Enter your last name:")
--- test/test_average_scores.py
+++ test/test_average_scores.py
@@ -2,12 +2,14 @@ import unittest
 from unittest import mock
 from format_output import average_scores
 
+
 class MyTestCase(unittest.TestCase):
     def test_average(self):
-        with mock.patch('builtins.input', side_effect=[85,90,95]):
+        with mock.patch('builtins.input', side_effect=[85, 90, 95]):
             assert average_scores.average() == 90
-        with mock.patch('builtins.input', side_effect=[60,70,80]):
+        with mock.patch('builtins.input', side_effect=[60, 70, 80]):
             assert average_scores.average() == 70
+
 
 if __name__ == '__main__':
     unittest.main()
